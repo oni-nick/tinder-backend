aed03d418ca97cf5702a9b55077c292dcb872ff1
Oni-Nick <nicholas00105@gmail.com>
2021-02-14 12:46:37 +0900

    After test server and db, before deploying with heroku

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import Cards from './dbCards.js'
+import Cors from "cors"
 // App Config
 const app = express();
 const port = process.env.PORT || 8001
 const connection_url = 'mongodb+srv://admin:xRL6vlIM8jpjWvK5@cluster0.ywwv8.mongodb.net/tinderdb?retryWrites=true&w=majority'
 // Middlewares
+app.use(express.json());
+app.use(Cors());
 
 // DB config
 mongoose.connect(connection_url, {
@@ -15,10 +18,28 @@ mongoose.connect(connection_url, {
 })
 // API Endpoints
 app.get('/', (req, res) => res.status(200).send('Hello World!!!'));
+// Put(Upload) data to our mongoDB
+app.post('/tinder/cards', (req, res) => {
+    const dbCard = req.body;
+    Cards.create(dbCard, (err, data) =>{
+        if(err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
 
-app.post('/tinder/card', (req, res) => {
+});
+app.get('/tinder/cards', (req, res) => {
+    Cards.find((err, data) =>{
+        if(err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
 
 });
 
-// Listener
+    // Listener
 app.listen(port, () => console.log('Listening on localhost: ' + port))
